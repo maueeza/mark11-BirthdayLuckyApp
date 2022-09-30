ac610d15cb6e1c5ab6327428f9ff2ab96c44b5dd
Maueeza Bhatkar <90929270+maueeza@users.noreply.github.com>
2022-09-30 12:04:19 +0530

    feat : added original functionality
    Added Javascript for functionality

--- functionality_mark11.js
+++ functionality_mark11.js
@@ -1,9 +1,14 @@
 var inpDate = document.querySelector("#date-input");
 var checkNoBtn = document.querySelector("#lucky-no-btn");
+const luckyNo = document.querySelector("#inp-lucky-no");
+var outputText = document.querySelector("#outputTextContainer");
+
 var sumOfNo = 0;
 
 function checkNoBtnHandler(){
     console.log(inpDate.value);
+    var inpLuckyNo = Number(luckyNo.value);
+    console.log("inpLuckyNo : ", inpLuckyNo+" typeof inpLuckyNo: "+typeof inpLuckyNo);
     var dateString = inpDate.value;
     const noString = dateString.replaceAll("-","");
     console.log(typeof noString);
@@ -16,6 +21,15 @@ function checkNoBtnHandler(){
         console.log("Sum Of Numbers in Birthdate : ",sumOfNo);
 
     }
+
+    if(sumOfNo%inpLuckyNo===0)
+    {
+        outputText.innerText = "Your Birthday is Lucky!";
+    }
+    else{
+        outputText.innerText = "Your Birthday is not Lucky!";
+    }
+    
 }
 
 checkNoBtn.addEventListener("click", checkNoBtnHandler);
